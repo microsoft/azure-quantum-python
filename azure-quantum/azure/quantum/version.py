4ecca400b204997c49c6fc2e64343041ae79789e
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-01-15 17:29:55 -0800

    add version.py to gitignore

--- azure-quantum/azure/quantum/version.py
+++
@@ -1,8 +0,0 @@
-# Auto-generated file, do not edit.
-##
-# version.py: Specifies the version of the azure.quantum package.
-##
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-##
-__version__ = "0.0.0.1"
