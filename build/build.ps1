8a8f618e6728107ff0291f1daa08e8cd3c2ff914
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-01-16 15:52:32 -0800

    move build and test script to powershell files

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Write-Host "##[info]Build QDK-Python package environment"
-# conda env create --quiet --file qdk/environment.yml
+Write-Host "##[info]Build QDK-Python package Conda environment"
+
+# Create conda environment
+conda env create --quiet --file environment.yml
--- build/test.ps1
+++ build/test.ps1
@@ -1,4 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Write-Host "##[info]Run QDK-Pyton tests"
-#pytest
+Write-Host "##[info]Install package in development mode and run QDK-Pyton tests"
+pip install -e .
+pytest
