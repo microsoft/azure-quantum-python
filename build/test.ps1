4ae10ea6550ac3150b81621c85ef37a609d2cf11
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-01-16 18:39:50 -0800

    use conda shell.powershell hook instead of conda init

--- build/test.ps1
+++ build/test.ps1
@@ -1,10 +1,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 param ($envName, $pkgDir)
-Write-Host "##[info]Install package in development mode and run tests for env $envName"
-conda init powershell
+Write-Host "##[info]Install package $pkgDir in development mode and run tests for env $envName"
+conda shell.powershell hook
+Write-Host "##[info]Activate $envName"
 conda activate $envName
-Write-Host "##[info]Installing ($envName), Python: $(which python)"
+which python
+Write-Host "##[info]Installing $pkgDir, Python: $(which python)"
 pip install -e $pkgDir
 Write-Host "##[info]Running pytest"
 pytest $pkgDir
