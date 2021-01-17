b9d3b4fbc0a2d236a343b6788bd664b50ea7007a
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-01-16 16:28:37 -0800

    add envName input var to test script

--- build/test.ps1
+++ build/test.ps1
@@ -1,5 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Write-Host "##[info]Install package in development mode and run QDK-Pyton tests"
+param ($envName)
+Write-Host "##[info]Install package in development mode and run tests for env $envName"
+conda activate $envName
+Write-Host "##[info]Installing ($envName), Python: $(which python)"
 pip install -e .
+Write-Host "##[info]Running pytest"
 pytest
