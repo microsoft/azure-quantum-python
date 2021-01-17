e2de07211450a36c20090de393319ec0e208a5f3
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-01-16 16:55:51 -0800

    add conda init powershell, run pytest on pkgDir

--- build/test.ps1
+++ build/test.ps1
@@ -2,8 +2,9 @@
 # Licensed under the MIT License.
 param ($envName, $pkgDir)
 Write-Host "##[info]Install package in development mode and run tests for env $envName"
+conda init powershell
 conda activate $envName
 Write-Host "##[info]Installing ($envName), Python: $(which python)"
 pip install -e $pkgDir
 Write-Host "##[info]Running pytest"
-pytest
+pytest $pkgDir
