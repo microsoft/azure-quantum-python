768d1c7e03fdb759716787b6e2ef8240d568f481
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-01-17 18:20:05 -0800

    add pkgDir input variable to build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -1,6 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Write-Host "##[info]Build QDK-Python package Conda environment"
+param ($pkgDir)
+
+if ($pkgDir -eq $null) {
+  Write-Host "##[info]No pkgDir. Setting to default 'qdk'"
+  $pkgDir = "qdk"
+}
+
+Write-Host "##[info]Build '$pkgDir' Conda environment"
 
 # Create conda environment
-conda env create --quiet --file environment.yml
+conda env create --quiet --file $pkgDir/environment.yml
