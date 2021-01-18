e38579636d39594b994b89515997be369d0f602a
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-01-18 09:26:09 -0800

    add pytest-azurepipelines

--- build/test.sh
+++ build/test.sh
@@ -3,5 +3,7 @@ echo "Run tests for env '$1', pkg dir '$2'"
 eval "$(conda shell.bash hook)"
 conda activate $1
 which python
+python -m pip install --upgrade pip
+pip install pytest pytest-azurepipelines
 pip install -e $2
 pytest $2
