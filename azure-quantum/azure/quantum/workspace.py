16373cc0fc838cac90ba3fc6e7a1c45a6d77fb1a
Guen P <guenp@microsoft.com>
2021-01-25 09:10:48 -0800

    Add end-to-end integration tests (#14)
    * add integration tests
    * client.sas_uri takes BlobDetails instead of container_name and blob_name
    * Add 1QBit solver

--- azure-quantum/azure/quantum/workspace.py
+++ azure-quantum/azure/quantum/workspace.py
@@ -18,6 +18,7 @@ from typing import List, Optional
 from msrest.authentication import Authentication, BasicTokenAuthentication
 from azure.quantum._client import QuantumClient
 from azure.quantum._client.operations import JobsOperations, StorageOperations
+from azure.quantum._client.models import BlobDetails
 from azure.quantum import Job
 try:
     from .version import __version__
@@ -46,14 +47,14 @@ else:
         BASE_URL                = lambda location: BASE_URL_FROM_ENV or f"https://eastus2euap.quantum.azure.com/"
     else:
         logger.debug("Using production configuration.")
-        BASE_URL                = lambda location: BASE_URL_FROM_ENV or f"https://{location}.quantum.azure.com/"        
+        BASE_URL                = lambda location: BASE_URL_FROM_ENV or f"https://{location}.quantum.azure.com/"
     ARM_BASE_URL            = "https://management.azure.com/"
     AAD_CLIENT_ID           = "84ba0947-6c53-4dd2-9ca9-b3694761521b" # Microsoft Quantum Development Kit
     AAD_SCOPES              = [ "https://quantum.microsoft.com/Jobs.ReadWrite" ]
- 
+
 TOKEN_CACHE = os.environ['AZURE_QUANTUM_TOKEN_CACHE'] if 'AZURE_QUANTUM_TOKEN_CACHE' in os.environ else os.path.join(Path.home(), ".azure-quantum", "aad.bin")
 
-# Keeps track of the account name the current token is associated, so we only show 
+# Keeps track of the account name the current token is associated, so we only show
 # a log message about acquiring a new token if the account changes.
 _last_account = None
 
@@ -321,7 +322,8 @@ class Workspace:
         Calls the service and returns a container sas url
         """
         client = self._create_workspace_storage_client()
-        container_uri = client.sas_uri(container_name, blob_name)
+        blob_details = BlobDetails(container_name=container_name, blob_name=blob_name)
+        container_uri = client.sas_uri(blob_details=blob_details)
 
         logger.debug(f"Container URI from service: {container_uri}")
         return container_uri.sas_uri
