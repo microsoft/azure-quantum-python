a472e3a09f00839b36aaa7a6302ef593b3d2d5d4
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-04-12 11:03:25 -0700

    Update setup.py

--- qdk/setup.py
+++ qdk/setup.py
@@ -39,6 +39,6 @@ setuptools.setup(
         'ruamel-yaml',
         'basis_set_exchange',
         'jupyter_nbextensions_configurator',
-        'pygments==2.7.4'
+        'pygments>=2.7.4'
     ]
 )
