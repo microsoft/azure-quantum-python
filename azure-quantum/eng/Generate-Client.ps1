8d5946a560a2b284de26196eb7a4954469a5459c
Victor XField <vxfield@outlook.com>
2021-04-15 15:50:49 -0700

    Scripts to re-generate autorest client

---
+++ azure-quantum/eng/Generate-Client.ps1
@@ -0,0 +1,42 @@
+$PackageName = "azure-quantum-_client"
+$PackageVersion = "0.0.1.0"
+$Namespace = "azure.quantum._client"
+
+$SpecsRepo = "https://github.com/Azure/azure-rest-api-specs.git"
+$SpecsBranch = "master"
+$SpecsCommitId = ""
+$PathAllowList = ("specification/quantum")
+
+$TempFolder = Join-Path $PSScriptRoot "../temp/"
+$SpecsFolder = Join-Path $TempFolder  "/specs/"
+
+$CheckoutScript = Join-Path $PSScriptRoot "./Checkout-Repo.ps1" -Resolve
+&$CheckoutScript -RepoUrl $SpecsRepo -TargetFolder $SpecsFolder -PathAllowList $PathAllowList -BranchName $SpecsBranch -CommitId $SpecsCommitId -Force | Write-Verbose
+
+$TempGeneratedClientFolder = Join-Path $TempFolder "generated"
+if (Test-Path $TempGeneratedClientFolder) {
+    Remove-Item $TempGeneratedClientFolder -Recurse | Write-Verbose
+}
+
+$AutoRestConfig = Join-Path $SpecsFolder "specification/quantum/data-plane/readme.md" -Resolve
+
+npm install -g autorest@latest | Write-Verbose
+
+autorest $AutoRestConfig `
+    --verbose `
+    --python `
+    --package-name=$PackageName `
+    --package-version=$PackageVersion `
+    --namespace=$Namespace `
+    --output-folder=$TempGeneratedClientFolder `
+    --no-namespace-folders=true `
+    --add-credential `
+    --python-mode=update `
+    | Write-Verbose
+
+$AzureQuantumClient_Folder =  Join-Path $PSScriptRoot "../azure/quantum/_client/"
+if (Test-Path $AzureQuantumClient_Folder) {
+    Remove-Item $AzureQuantumClient_Folder -Recurse | Write-Verbose
+}
+
+Copy-Item $TempGeneratedClientFolder $AzureQuantumClient_Folder -Recurse | Write-Verbose
