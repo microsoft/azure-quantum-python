754bba18165213458fa85d3eba71ecb939de845c
Sanjana Gupta <42555338+sanjgupt@users.noreply.github.com>
2021-05-03 16:32:16 -0700

    pep8: onequibit/solvers

--- azure-quantum/azure/quantum/optimization/oneqbit/solvers.py
+++ azure-quantum/azure/quantum/optimization/oneqbit/solvers.py
@@ -3,13 +3,10 @@
 # Licensed under the MIT License.
 ##
 import logging
-import uuid
-import azure.quantum
 
-from typing import List, Union, Any, Optional
-from enum import Enum
+from typing import Optional, List
 from azure.quantum import Workspace
-from azure.quantum.optimization import Problem, Solver
+from azure.quantum.optimization import Solver
 
 logger = logging.getLogger(__name__)
 
@@ -31,7 +28,8 @@ class TabuSearch(Solver):
         An iterative heuristic algorithm that uses local search techniques
         to solve a QUBO problem. It starts from a random solution and looks
         for an improved solution in the solution's neighborhood which includes
-        all possible single flips. The algorithm stops when it reaches a stopping
+        all possible single flips.
+        The algorithm stops when it reaches a stopping
         criterion, such as a specified number of consecutive iterations without
         improvement.
 
@@ -39,19 +37,22 @@ class TabuSearch(Solver):
         https://portal.1qbit.com/docs/model/tabusolver
 
         :param improvement_cutoff:
-            The number of iterations that the solver attempts with no improvement
+            The number of iterations that
+            the solver attempts with no improvement
             before stopping. Default: 0
         :param improvement_tolerance:
             The tolerance value that determines if a solution is an improvement
             over the previous iteration. Default: 1e-9
         :param tabu_tenure:
-            The tenure prevents a flipped variable from being flipped again during
+            The tenure prevents a flipped
+            variable from being flipped again during
             the iterations. Default: 0
         :param tabu_tenure_rand_max:
             The upper limit of the exclusive range of random integers.
             Valid value range: 1 to 200,000. Default: 0
         :param timeout:
-            The duration in ms the solver runs before exiting. If the value is set
+            The duration in ms the solver
+            runs before exiting. If the value is set
             to 0, it does not time out. Default: 0
         """
         super().__init__(
@@ -97,12 +98,16 @@ class PticmSolver(Solver):
     ):
         """The constructor of a PTICM solver.
 
-        The parallel tempering with isoenergetic cluster moves (PTICM) solver is
-        a Monte Carlo approach to solving QUBO problems. In this algorithm, multiple
-        replicas of the original system, each with a different initial state, are
+        The parallel tempering with isoenergetic
+        cluster moves (PTICM) solver is
+        a Monte Carlo approach to solving QUBO problems.
+        In this algorithm, multiple
+        replicas of the original system,
+        each with a different initial state, are
         simulated at different temperatures simultaneously. The replicas at
         neighboring temperatures are periodically swapped based on a Metropolis
-        criterion. These swaps allow different replicas to do a random walk in the
+        criterion. These swaps allow
+        different replicas to do a random walk in the
         temperature space, thereby, efficiently overcoming energy barriers.
 
         For more information please visit:
@@ -110,7 +115,8 @@ class PticmSolver(Solver):
 
         :param auto_set_temperatures:
             This defines whether the temperature parameters are auto-calculated
-            or not. Set it to True for auto-calculating and False for customizing
+            or not. Set it to True for
+            auto-calculating and False for customizing
             the temperature parameters. Default: True
         :param elite_threshold:
             The fraction of the best solutions used for the var_fixing_type
@@ -161,14 +167,19 @@ class PticmSolver(Solver):
             This defines whether or not to perform the isoenergetic cluster
             moves. Default: True
         :param scaling_type:
-            This defines whether the QUBO problem is automatically scaled or not.
-            "MEDIAN" means it's automatically scaled, and "NO_SCALING" means it's
+            This defines whether the QUBO problem
+            is automatically scaled or not.
+            "MEDIAN" means it's automatically
+            scaled, and "NO_SCALING" means it's
             not. Valid values: "MEDIAN" or "NO_SCALING"
         :param var_fixing_type:
             This decides whether the values of QUBO variables are fixed or not.
-            You can fix them with "PERSISTENCY" or "SPVAR" types. "NO_FIXING" means the
-            variables are not fixed. If you choose "PERSISTENCY" or "SPVAR", also
-            set the frac_sweeps_fixing and frac_sweeps_idle parameters to a number
+            You can fix them with "PERSISTENCY" or
+            "SPVAR" types. "NO_FIXING" means the
+            variables are not fixed. If you
+            choose "PERSISTENCY" or "SPVAR", also
+            set the frac_sweeps_fixing and
+            frac_sweeps_idle parameters to a number
             less than one. Valid values: "PERSISTENCY", "SPVAR" or "NO_FIXING".
             Default: "NO_FIXING"
         """
@@ -234,7 +245,8 @@ class PathRelinkingSolver(Solver):
             improvement. Valid values: 0.0 to 0.5. Default: 0.33
         :param greedy_path_relinking:
             When you use the path-relinking solver there are two ways you can
-            generate a path that leads to the solution: one is the greedy function
+            generate a path that leads to the solution:
+            one is the greedy function
             and the other operates in a random matter. If you set the this
             parameter to true, the solver will use the greedy function. If you
             set it to false, it will use the random method. Default: False
@@ -242,7 +254,8 @@ class PathRelinkingSolver(Solver):
             The number of initial elite solutions to be generated by the tabu
             search algorithm. Valid values: Greater than 1. Default: 10
         :param timeout:
-            The duration in ms the solver runs before exiting. If the value is set
+            The duration in ms the solver runs
+            before exiting. If the value is set
             to 0, it does not time out. Default: 0
         """
         super().__init__(
