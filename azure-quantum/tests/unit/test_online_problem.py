35620ab2d9b3c11ee6457066e35d3f9035563e9c
Sanjana Gupta <42555338+sanjgupt@users.noreply.github.com>
2021-05-03 11:42:46 -0700

    add another assertion for online problems

--- azure-quantum/tests/unit/test_online_problem.py
+++ azure-quantum/tests/unit/test_online_problem.py
@@ -11,12 +11,14 @@ class TestOnlineProblemClass(unittest.TestCase):
     def setUp(self):
         self.o_problem = OnlineProblem(name = "test", blob_uri = "mock_blob_uri")
     
+    
     def test_download(self):
         azure.quantum.optimization.problem.download_blob = Mock(return_value = expected_terms())
         acutal_result = self.o_problem.download()
         #to-do add test that user warning was registered in log
         assert acutal_result.name == 'test'
         azure.quantum.optimization.problem.download_blob.assert_called_once()
+        assert isinstance(acutal_result, Problem)
 
     def test_evaluate(self):
         config_dict = {
