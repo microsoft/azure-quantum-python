f5bd918572c56c4092de50a39090184aaca7a69d
Victor XField <vxfield@outlook.com>
2021-05-18 02:41:29 -0700

    Enable online problem test case

--- azure-quantum/tests/unit/test_online_problem.py
+++ azure-quantum/tests/unit/test_online_problem.py
@@ -6,23 +6,21 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 ##
-'''
-import unittest
 from unittest.mock import Mock
 from typing import TYPE_CHECKING
 from azure.quantum.optimization import Problem, Term, OnlineProblem
 import azure.quantum.optimization.problem
-from common import expected_terms
+from common import expected_terms, QuantumTestBase
 import json
 
 
-class TestOnlineProblemClass(unittest.TestCase):
+class TestOnlineProblemClass(QuantumTestBase):
     def setUp(self):
         self.mock_ws = Mock()
         self.mock_ws._get_linked_storage_sas_uri.return_value = Mock()
         self.o_problem = OnlineProblem(name="test", blob_uri="mock_blob_uri")
 
-    @pytest.skip(reason= "OnlineProblem not available in SDK yet")
+    # TODO: instead of using mock, connect to a live service and record the responses
     def test_download(self):
         azure.quantum.optimization.problem.download_blob = Mock(
             return_value=expected_terms()
@@ -31,9 +29,8 @@ class TestOnlineProblemClass(unittest.TestCase):
         azure.quantum.optimization.problem.BlobClient.from_blob_url.return_value = Mock()
         azure.quantum.optimization.problem.ContainerClient = Mock()
         azure.quantum.optimization.problem.ContainerClient.from_container_url.return_value = Mock()
-        acutal_result = self.o_problem.download(self.mock_ws)
-        # to-do add test that user warning was registered in log
-        assert acutal_result.name == "test"
+        actual_result = self.o_problem.download(self.mock_ws)
+        # TODO: add test that user warning was registered in log
+        assert actual_result.name == "test"
         azure.quantum.optimization.problem.download_blob.assert_called_once()
-        assert isinstance(acutal_result, Problem)
-'''
+        assert isinstance(actual_result, Problem)
