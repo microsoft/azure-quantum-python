b79286c838589d10c0ba9ec1192063a281543365
Victor XField <vxfield@outlook.com>
2021-05-17 20:15:52 -0700

    Add credential scope to generate client script

--- azure-quantum/eng/Generate-Client.ps1
+++ azure-quantum/eng/Generate-Client.ps1
@@ -10,18 +10,22 @@ $PathAllowList = ("specification/quantum")
 $TempFolder = Join-Path $PSScriptRoot "../temp/"
 $SpecsFolder = Join-Path $TempFolder  "/specs/"
 
-$CheckoutScript = Join-Path $PSScriptRoot "./Checkout-Repo.ps1" -Resolve
-# &$CheckoutScript -RepoUrl $SpecsRepo -TargetFolder $SpecsFolder -PathAllowList $PathAllowList -BranchName $SpecsBranch -CommitId $SpecsCommitId -Force | Write-Verbose
+# Check-out specs repo to get the latest swagger API Definition file
+$CheckoutScript = Join-Path $PSScriptRoot "./Checkout-Repo.ps1" 
+&$CheckoutScript -RepoUrl $SpecsRepo -TargetFolder $SpecsFolder -PathAllowList $PathAllowList -BranchName $SpecsBranch -CommitId $SpecsCommitId -Force | Write-Verbose
 
+# Delete the old generated client in the temp folder
 $TempGeneratedClientFolder = Join-Path $TempFolder "generated/_client/"
 if (Test-Path $TempGeneratedClientFolder) {
     Remove-Item $TempGeneratedClientFolder -Recurse | Write-Verbose
 }
 
-$AutoRestConfig = Join-Path $SpecsFolder "specification/quantum/data-plane/readme.md" -Resolve
+$AutoRestConfig = Join-Path $SpecsFolder "specification/quantum/data-plane/readme.md"
 
+# Make sure we have the latest AutoRest
 npm install -g autorest@latest | Write-Verbose
 
+# Generate the client in a temp folder
 autorest $AutoRestConfig `
     --verbose `
     --python `
@@ -30,13 +34,14 @@ autorest $AutoRestConfig `
     --namespace=$Namespace `
     --no-namespace-folders=true `
     --add-credential `
+    --credential-scopes="https://quantum.microsoft.com/.default" `
     --python-mode=custom `
     --output-folder=$TempGeneratedClientFolder `
     | Write-Verbose
 
+# Delete the old generated client and copy the new one there
 $AzureQuantumClient_Folder =  Join-Path $PSScriptRoot "../azure/quantum/_client/"
 if (Test-Path $AzureQuantumClient_Folder) {
     Remove-Item $AzureQuantumClient_Folder -Recurse | Write-Verbose
 }
-
 Copy-Item $TempGeneratedClientFolder $AzureQuantumClient_Folder -Recurse -Force | Write-Verbose
