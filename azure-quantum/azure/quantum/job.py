18a5be0b3a31d2e9865f532ab65a43bb01f13970
Adele Bai <adbai@microsoft.com>
2021-07-16 12:16:06 -0700

    supporting date object and using correct timezone convert

--- azure-quantum/azure/quantum/job.py
+++ azure-quantum/azure/quantum/job.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 ##
-from datetime import datetime, timezone
+from datetime import date, datetime, timezone
 import logging
 import time
 import json
@@ -119,8 +119,17 @@ class Job:
         if status is not None and self.details.status != status.value:
             return False
         
-        if created_after is not None and self.details.creation_time.replace(tzinfo=timezone.utc) < created_after.replace(tzinfo=timezone.utc):
-            return False
+        if created_after is not None:
+            # if supplied date is date we must convert to datetime first
+            if type(created_after) is date:
+                created_after = datetime(created_after.year, created_after.month, created_after.day)
+            
+            # if supplied date is naive, assume local and convert to timezone aware object
+            if created_after.tzinfo is None:
+                created_after = created_after.astimezone()
+            
+            if self.details.creation_time.replace(tzinfo=timezone.utc) < created_after:
+                return False
 
         return True
 
