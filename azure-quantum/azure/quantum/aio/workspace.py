59c40920de43b026f1b23a2dfa5966f836917840
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-07-20 11:29:05 +1000

    Add the authentication submodule

--- azure-quantum/azure/quantum/aio/workspace.py
+++ azure-quantum/azure/quantum/aio/workspace.py
@@ -5,15 +5,17 @@
 import asyncio
 from datetime import datetime
 import logging
+import re
 
 from typing import List, Optional
 
+from azure.quantum._authentication._default import _DefaultAzureCredential
 from azure.quantum._client.aio import QuantumClient
 from azure.quantum._client.aio.operations import JobsOperations, StorageOperations
 from azure.quantum._client.models import BlobDetails, JobStatus
 from azure.quantum.aio.job import Job
 
-from azure.quantum.workspace import Workspace as SyncWorkspace, BASE_URL
+from azure.quantum.workspace import Workspace as SyncWorkspace, BASE_URL, ARM_BASE_URL
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +23,108 @@ __all__ = ["Workspace"]
 
 
 class Workspace(SyncWorkspace):
+    """Represents an Azure Quantum workspace.
+
+    When creating a Workspace object, callers have two options for identifying
+    the Azure Quantum workspace:
+    1. specify a valid resource ID, or
+    2. specify a valid subscription ID, resource group, and workspace name.
+
+    If the Azure Quantum workspace does not have linked storage, the caller
+    must also pass a valid Azure storage account connection string.
+
+    :param subscription_id:
+        The Azure subscription ID. Ignored if resource_id is specified.
+
+    :param resource_group:
+        The Azure resource group name. Ignored if resource_id is specified.
+
+    :param name:
+        The Azure Quantum workspace name. Ignored if resource_id is specified.
+
+    :param storage:
+        The Azure storage account connection string.
+        Required only if the specified Azure Quantum
+        workspace does not have linked storage.
+
+    :param resource_id:
+        The resource ID of the Azure Quantum workspace.
+
+    :param location:
+        The Azure region where the Azure Quantum workspace is provisioned.
+        This may be specified as a region name such as
+        \"East US\" or a location name such as \"eastus\".
+
+    :param credential:
+        The credential to use to connect to Azure services.
+        Normally one of the credential types from Azure.Identity (https://docs.microsoft.com/en-us/python/api/overview/azure/identity-readme?view=azure-python#credential-classes).
+
+        Defaults to \"DefaultAzureCredential\", which will attempt multiple 
+        forms of authentication.
+    """
+
+    credentials = None
+
+    def __init__(
+        self,
+        subscription_id: Optional[str] = None,
+        resource_group: Optional[str] = None,
+        name: Optional[str] = None,
+        storage: Optional[str] = None,
+        resource_id: Optional[str] = None,
+        location: Optional[str] = None,
+        credential: Optional[object] = None,
+    ):
+        if resource_id is not None:
+            # A valid resource ID looks like:
+            # /subscriptions/f846b2bd-d0e2-4a1d-8141-4c6944a9d387/resourceGroups/
+            # RESOURCE_GROUP_NAME/providers/Microsoft.Quantum/Workspaces/WORKSPACE_NAME
+            regex = r"^/subscriptions/([a-fA-F0-9-]*)/resourceGroups/([^\s/]*)/providers/Microsoft\.Quantum/Workspaces/([^\s/]*)$"
+            match = re.search(regex, resource_id, re.IGNORECASE)
+            if match:
+                # match should contain four groups:
+                # -> match.group(0):
+                # The full resource ID for the Azure Quantum workspace
+                # -> match.group(1): The Azure subscription ID
+                # -> match.group(2): The Azure resource group name
+                # -> match.group(3): The Azure Quantum workspace name
+                subscription_id = match.group(1)
+                resource_group = match.group(2)
+                name = match.group(3)
+
+        if not subscription_id or not resource_group or not name:
+            raise ValueError(
+                "Azure Quantum workspace not fully specified."
+                + "Please specify either a valid resource ID "
+                + "or a valid combination of subscription ID,"
+                + "resource group name, and workspace name."
+            )
+
+        if not location:
+            raise ValueError(
+                "Azure Quantum workspace does not have an associated location. " +
+                "Please specify the location associated with your workspace.")
+
+        # Temporarily using a custom _DefaultAzureCredential
+        # instead of Azure.Identity.DefaultAzureCredential
+        # See _DefaultAzureCredential documentation for more info.
+        if credential is None:
+            credential = _DefaultAzureCredential(exclude_interactive_browser_credential=False,
+                                                 subscription_id=subscription_id,
+                                                 arm_base_url=ARM_BASE_URL)
+
+        self.credentials = credential
+        self.name = name
+        self.resource_group = resource_group
+        self.subscription_id = subscription_id
+        self.storage = storage
+
+        # Convert user-provided location into names
+        # recognized by Azure resource manager.
+        # For example, a customer-provided value of
+        # "West US" should be converted to "westus".
+        self.location = "".join(location.split()).lower()
+
 
     def _create_client(self) -> QuantumClient:
         base_url = BASE_URL(self.location)
