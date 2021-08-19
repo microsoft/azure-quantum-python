3cb437fabf2c12a11422e76613e0cf6c4b35db0d
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-08-18 19:49:18 -0700

    update inline comment

--- azure-quantum/azure/quantum/target/__init__.py
+++ azure-quantum/azure/quantum/target/__init__.py
@@ -19,7 +19,8 @@ from azure.quantum.target.optimization.toshiba import (
     SimulatedBifurcationMachine
 )
 
-# Default targets to use when there is no associated target class
+# Default targets to use when there is no target class
+# associated with a given target ID
 DEFAULT_TARGETS = {
     "ionq": IonQ,
     "honeywell": Honeywell,
