796f9a3b6d767d0153883ba3467d277e96015161
Guen Prawiroatmodjo (SHE/HER) <guenp@microsoft.com>
2021-08-20 09:46:49 -0700

    add missing copyright

--- azure-quantum/azure/quantum/target/microsoft/qio/__init__.py
+++ azure-quantum/azure/quantum/target/microsoft/qio/__init__.py
@@ -1,3 +1,7 @@
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
 from .parallel_tempering import ParallelTempering
 from .population_annealing import PopulationAnnealing
 from .quantum_monte_carlo import QuantumMonteCarlo
