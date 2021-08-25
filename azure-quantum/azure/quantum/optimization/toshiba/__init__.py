d6a22b9a948de455b6278abcc835e0b3159b99e1
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-08-25 12:37:08 -0700

    Add deprecation notice to 3rd party QIO namespaces (#132)
    * add deprecation notice to 3rd party QIO namespaces
    
    * add solvers.py
    
    * add more specific deprecation warning
    
    * remove redundant legacy imports

---
+++ azure-quantum/azure/quantum/optimization/toshiba/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+import warnings
+warnings.warn("The azure.quantum.optimization.toshiba namespace will be deprecated. \
+Please use azure.quantum.target.toshiba instead.")
+
+from .solvers import *
---
+++ azure-quantum/azure/quantum/optimization/toshiba/solvers.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+import warnings
+warnings.warn("The azure.quantum.optimization.toshiba.solvers namespace will be deprecated. \
+Please use azure.quantum.target.toshiba instead.")
+
+from azure.quantum.target.toshiba.solvers import (
+    SimulatedBifurcationMachine
+)
