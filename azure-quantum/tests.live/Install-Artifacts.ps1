e5008ec15a1155f42876fef691339835fc3ff460
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-09-27 16:38:18 -0700

    add install from artifacts

--- azure-quantum/tests.live/Install-Artifacts.ps1
+++ azure-quantum/tests.live/Install-Artifacts.ps1
@@ -3,12 +3,33 @@
 
 <#
     .SYNOPSIS
-        Bootstrap: set up a Python environment using Anaconda
+        Install-Artifacts: set up a Python environment using Anaconda
 #>
 
 $PackageDir = Split-Path -parent $PSScriptRoot;
 $RootDir = Split-Path -parent $PackageDir;
 Import-Module (Join-Path $RootDir "build" "conda-utils.psm1");
+
+<# 
+    Install From Source
+#>
+function Install-FromSource() {
+    & (Join-Path $RootDir build build.ps1) -PackageDirs $PackageDir
+}
+
+<# 
+    Install From Build Artifacts
+#>
+function Install-FromBuild() {
+    Push-Location $Env:PYTHON_OUTDIR
+        "Installing $PackageDir wheels from $Env:PYTHON_OUTDIR" | Write-Verbose
+        Get-ChildItem $PackageDir*.whl `
+        | ForEach-Object {
+            "Installing $_.Name" | Write-Verbose
+            pip install --verbose --verbose $_.Name
+        }
+    Pop-Location
+}
 
 # Enable conda hook
 Enable-Conda
@@ -24,5 +45,30 @@ if ($EnvExists -eq "1") {
     & (Join-Path $RootDir build create-env.ps1) -PackageDirs $PackageDir
 }
 
-# Install package from source
-& (Join-Path $RootDir build build.ps1) -PackageDirs $PackageDir
+if (-not $Env:PYTHON_OUTDIR) {
+    "" | Write-Host
+    "== Environment variable $Env:PYTHON_OUTDIR is not set. " | Write-Host
+    "== We will install $PackageDir from source." | Write-Host
+    "" | Write-Host
+
+    Install-FromSource
+
+    "" | Write-Host
+    "== $PackageDir installed from source. ==" | Write-Host
+    "" | Write-Host
+} elseif (-not (Test-Path $Env:PYTHON_OUTDIR)) {
+    "" | Write-Warning
+    "== The environment variable PYTHON_OUTDIR is set, but pointing to an invalid location ($Env:PYTHON_OUTDIR)" | Write-Warning
+    "== To use build artifacts, download the artifacts locally and point the variable to this folder." | Write-Warning
+    "" | Write-Warning
+    Exit 1
+} else {
+    "== Preparing environment to use artifacts with version '$Env:PYTHON_VERSION' " | Write-Host
+    "== from '$Env:PYTHON_OUTDIR'" | Write-Host
+    
+    Install-FromBuild
+    
+    "" | Write-Host
+    "== $PackageDir installed from build artifacts. ==" | Write-Host
+    "" | Write-Host
+}
