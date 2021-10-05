7c2fcee9c186eb7bb543fcf5a44488a3dbaa870a
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-10-05 11:08:19 -0700

    Refactor CI/CD steps, add tests against cirq beta (#151)
    * refactor pipeline
    * test cirq beta release
    * clean up reused code
    * add package-utils module

--- build/pack.ps1
+++ build/pack.ps1
@@ -3,79 +3,38 @@
 
 <#
     .SYNOPSIS
-        Pack: create wheels for given packages in given environments, output to directory
+        Pack: create wheels for given packages in given environments, output to directory.
+        If no PackageName is specified, this script searches the root directory and runs
+        the wheel packaging for all packages.
 #>
 
 param (
-  [string[]] $PackageDirs,
-  [string[]] $EnvNames,
+  [string] $PackageName,
   [string] $OutDir
 )
 
 & (Join-Path $PSScriptRoot "set-env.ps1");
 
-Import-Module (Join-Path $PSScriptRoot "conda-utils.psm1");
-
-if ($null -eq $PackageDirs) {
-  $parentPath = Split-Path -parent $PSScriptRoot
-  $PackageDirs = Get-ChildItem -Path $parentPath -Recurse -Filter "environment.yml" | Select-Object -ExpandProperty Directory | Split-Path -Leaf
-  Write-Host "##[info]No PackageDir. Setting to default '$PackageDirs'"
-}
-
-if ($null -eq $EnvNames) {
-  $EnvNames = $PackageDirs | ForEach-Object {$_.replace("-", "")}
-  Write-Host "##[info]No EnvNames. Setting to default '$EnvNames'"
-}
+Import-Module (Join-Path $PSScriptRoot "package-utils.psm1");
 
 if ($OutDir -eq "") {
   Write-Host "##[info]No OutDir. Setting to env var $Env:PYTHON_OUTDIR"
   $OutDir = $Env:PYTHON_OUTDIR
 }
 
-# Check that input is valid
-if ($EnvNames.length -ne $PackageDirs.length) {
-  throw "Cannot run build script: '$EnvNames' and '$PackageDirs' lengths don't match"
-}
-
 $script:all_ok = $True
-
-function Create-Wheel() {
-  param(
-    [string] $EnvName,
-    [string] $Path,
-    [string] $OutDir
-  );
-
-  Push-Location $Path
-    # Set environment vars to be able to run conda activate
-    Write-Host "##[info]Pack wheel for env '$EnvName'"
-    # Activate env
-    Use-CondaEnv $EnvName
-    # Create package distribution
-    python setup.py bdist_wheel sdist --formats=gztar
-
-    if  ($LastExitCode -ne 0) {
-      Write-Host "##vso[task.logissue type=error;]Failed to build $Path."
-      $script:all_ok = $False
-    } else {
-      if ($OutDir -ne "") { 
-        Write-Host "##[info]Copying wheel to '$OutDir'"
-        Copy-Item "dist/*.whl" $OutDir/
-        Copy-Item "dist/*.tar.gz" $OutDir/
-      }
-    }
-  Pop-Location
-}
 
 if ($Env:ENABLE_PYTHON -eq "false") {
     Write-Host "##vso[task.logissue type=warning;]Skipping Creating Python packages. Env:ENABLE_PYTHON was set to 'false'."
 } else {
     python --version
     $parentPath = Split-Path -parent $PSScriptRoot
+    $PackageNames = PackagesList -PackageName $PackageName
 
-    for ($i=0; $i -le $PackageDirs.length-1; $i++) {
-      $PackageDir = Join-Path $parentPath $PackageDirs[$i]
-      Write-Host "##[info]Packing Python wheel in env '$EnvNames[$i]' for '$PackageDir' to '$OutDir'..."
-      Create-Wheel -EnvName $EnvNames[$i] -Path $PackageDir -OutDir $OutDir
+    foreach ($PackageName in $PackageNames) {
+      $EnvName = $PackageName.replace("-", "")
+      $AbsPath = Join-Path $parentPath $PackageName
+      Write-Host "##[info]Packing Python wheel in env '$EnvName' for '$PackageName' to '$OutDir'..."
+      New-Wheel -EnvName $EnvName -Path $AbsPath -OutDir $OutDir
     }
 }
