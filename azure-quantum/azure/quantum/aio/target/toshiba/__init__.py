f33d106db9f49942e12573d8e0905a399221d8c7
XField <58103249+vxfield@users.noreply.github.com>
2021-10-20 17:46:09 -0700

    Revert Async Target changes

--- azure-quantum/azure/quantum/aio/target/toshiba/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-##
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-##
-
-from azure.quantum.target.toshiba import (
-    SimulatedBifurcationMachine
-)
