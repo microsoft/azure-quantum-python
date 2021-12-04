12a71be902dab6297ff76e51e2a2107e5e00c41b
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-12-03 18:48:28 -0800

    add examples for how to estimate job costs for Honeywell and IonQ

---
+++ examples/estimate-job-cost/estimate_honeywell.py
@@ -0,0 +1,72 @@
+# %% Get all one-qubit and two-qubit gates
+from qiskit import QuantumCircuit
+from qiskit.converters import circuit_to_dag
+
+
+GATES_1Q = [
+    "x",
+    "y",
+    "z",
+    "rx",
+    "ry",
+    "rz",
+    "h",
+    "s",
+    "sdg",
+    "t",
+    "tdg",
+    "v",
+    "vdg",
+]
+
+GATES_MULTI = [
+    "cx",
+    "ccx",
+    "cz",
+    "zz",
+]
+
+GATES_M = [
+    "measure",
+    "reset"
+]
+
+
+def estimate_cost_honeywell(qasm_str: str, num_shots: int) -> float:
+    """Estimate cost for QASM job on Honeywell
+
+    :param qasm_str: QASM description
+    :type qasm_str: str
+    :param num_shots: Number of shots
+    :type num_shots: int
+    :return: Cost estimation
+    :rtype: float
+    """
+    qc = QuantumCircuit.from_qasm_str(qasm_str)
+    dag = circuit_to_dag(qc)
+    ops = dag.count_ops()
+    N_1q = sum([value for key, value in ops.items() if key in GATES_1Q])
+    N_2q = sum([value for key, value in ops.items() if key in GATES_MULTI])
+    N_m = sum([value for key, value in ops.items() if key in GATES_M])
+    HQC = 5 + num_shots * (N_1q + 10 * N_2q + 5 * N_m) / 5000
+    return HQC
+
+
+test_circuit = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[4];
+creg c[4];
+h q[0];
+h q[1];
+h q[2];
+h q[3];
+cx q[0],q[1];
+cx q[2],q[3];
+ry(2) q[0];
+cx q[1],q[0];
+measure q[0] -> c[0];
+measure q[1] -> c[1];
+measure q[2] -> c[2];
+measure q[3] -> c[3];"""
+
+estimate_cost_honeywell(test_circuit, 10)
---
+++ examples/estimate-job-cost/estimate_ionq.py
@@ -0,0 +1,94 @@
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+
+# %% Get all one-qubit and two-qubit gates
+from typing import Dict, Any
+
+GATES_1Q = [
+    "x",	# Pauli X gate
+    "y",	# Pauli Y gate
+    "z",	# Pauli Z gate
+    "rx",	# X-axis rotation
+    "ry",	# Y-axis rotation
+    "rz",	# Z-axis rotation
+    "h",	# Hadamard gate
+    "not",	# Convenient alias for Pauli-X gate
+    "s",	# S gate
+    "si",	# Conjugate transpose of S gate
+    "t",	# T gate
+    "ti",	# Conjugate transpose of T gate
+    "v",	# Square root of not gate
+    "vi",	# Conjugate transpose of square-root-of-not gate
+]
+
+GATES_MULTI = [
+    "cnot",	# Convenient alias for controlled-not gate
+    "xx",	# Ising XX gate: e^(-iθ X⊗X /2)
+    "yy",	# Ising YY gate: e^(-iθ Y⊗Y /2)
+    "zz",	# Ising ZZ gate: e^(-iθ Z⊗Z /2)
+    "swap",	# Swaps two qubits
+]
+
+def is_1q_gate(gate: Dict[str, Any]):
+    return gate.get("gate") in GATES_1Q
+
+def is_multi_q_gate(gate):
+    return gate.get("gate") in GATES_MULTI
+
+def num_2q_gates(gate):
+    controls = gate.get("controls")
+    if controls is None:
+        # Only one control qubit
+        return 1
+    # Multiple control qubits
+    return 6 * (len(controls) - 2)
+
+def estimate_cost_ionq(
+    circuit: Dict[str, Any],
+    num_shots: int,
+    cost_1q: float=0.00003,
+    cost_2q: float=0.0003
+) -> float:
+    """Estimate the costs of running a circuit on IonQ.
+
+    :param circuit: IonQ JSON circuit
+    :type circuit: Dict[str, Any]
+    :param num_shots: Number of shots
+    :type num_shots: int
+    :param cost_1q: Cost for a single qubit gate-shot, defaults to 0.00003
+    :type cost_1q: float, optional
+    :param cost_2q: Cost for a double qubit gate-shot, defaults to 0.0003
+    :type cost_2q: float, optional
+    :return: Calculate the cost for running the circuit
+    :rtype: float
+    """
+    gates = circuit.get("circuit", [])
+    N_1q = sum(map(is_1q_gate, gates))
+    N_2q = sum(map(num_2q_gates, filter(is_multi_q_gate, gates)))
+    cost = (cost_1q * N_1q + cost_2q * N_2q) * num_shots
+    return max(cost, 1.0)
+
+# %% Estimate costs for a test circuit
+test_circuit = {
+            "qubits": 3,
+            "circuit": [
+                {
+                "gate": "h",
+                "target": 0
+                },
+                {
+                "gate": "cnot",
+                "control": 0,
+                "target": 1
+                },
+                {
+                "gate": "cnot",
+                "control": 0,
+                "target": 2
+                },
+            ]
+        }
+
+estimate_cost_ionq(circuit=test_circuit, num_shots=1024)
