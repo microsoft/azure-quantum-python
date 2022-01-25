858b0820575520bad22d02cba4e325b40fdaa516
Sai Seshu Chadaram <sachadar@microsoft.com>
2022-01-25 19:06:59 +0530

    Fix dirty atexit for IonQ backend

--- azure-quantum/azure/quantum/projectq/backends/ionq.py
+++ azure-quantum/azure/quantum/projectq/backends/ionq.py
@@ -73,6 +73,10 @@ class AzureIonQBackend(_IonQBackend):
 
     def submit_job(self, name=None, **kwargs) -> Job:
         """Submits the given circuit to run on an IonQ target."""
+        if not self._circuit:
+            logger.debug("Cannot run circuit because it is empty.")
+            return
+            
         logger.info(f"Submitting new job for backend {self.device}")
 
         if name is None:
@@ -118,16 +122,15 @@ class AzureIonQBackend(_IonQBackend):
         logger.info(f"Submitted job with id '{job.id}' for circuit '{name}':")
         logger.info(input_data)
 
+        # reset engine state
+        self._reset()
+
         return job
 
     def _run(self):
         """
         Run a ProjectQ circuit and wait until it is done.
         """
-        if not self._circuit:
-            logger.debug("Cannot run circuit because it is empty.")
-            return
-
         job = self.submit_job()
         result = job.get_results()
         self._probabilities = {int_to_bitstring(k, len(self._measured_ids), self._measured_ids): v for k, v in result["histogram"].items()}
