8b403eff88e6749b22449cdd6f3201b982039bb2
Guen Prawiroatmodjo <guenp@microsoft.com>
2022-02-06 23:59:53 -0800

    move .ll files to /unit/qir, add Qiskit sample and UT

--- azure-quantum/tests/unit/test_simulator.py
+++ azure-quantum/tests/unit/test_simulator.py
@@ -17,7 +17,10 @@ class TestFullStateSimulator(QuantumTestBase):
                else Job.create_job_id()
 
     def _test_qir_file(self):
-        return os.path.join(os.path.split(__file__)[0], "Sample.ll")
+        return os.path.join(os.path.split(__file__)[0], "qir", "Sample.ll")
+
+    def _test_qir_qiskit_file(self):
+        return os.path.join(os.path.split(__file__)[0], "qir", "QiskitSample.ll")
 
     def test_job_submit_microsoft_full_state(self):
         with unittest.mock.patch.object(
@@ -30,3 +33,15 @@ class TestFullStateSimulator(QuantumTestBase):
             job = target.submit_qir_file(self._test_qir_file(), "QIR test", "Sample__HelloQ")
             result: str = job.get_results()
             assert result.startswith("Hello quantum world!")
+
+    def test_job_submit_qiskit_microsoft_full_state(self):
+        with unittest.mock.patch.object(
+            Job,
+            self.mock_create_job_id_name,
+            return_value=self.get_test_job_id(),
+        ):
+            workspace = self.create_workspace()
+            target: FullStateTarget = workspace.get_targets("microsoft.simulator.fullstate")
+            job = target.submit_qir_file(self._test_qir_qiskit_file(), "Qiskit GHZ program", "QuantumApplication__Run")
+            with pytest.raises(RuntimeError):
+                job.get_results()
