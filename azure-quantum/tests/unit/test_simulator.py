85654ffa16798c939f5882729090a386184aa7fd
Guen Prawiroatmodjo <guenp@microsoft.com>
2022-03-07 10:12:32 -0800

    Add FullStateTarget that submits QIR payload (#233)
    * raw qir sample
    
    * clean-up
    
    * Adding workitems
    
    * Align PassthroughTarget with Target
    
    * remove target's submit implementation
    
    * Add FullStateTarget, add UT
    
    * UT fails with JSONDecodeError
    
    * try to convert payload to json, return otherwise
    
    * move .ll files to /unit/qir, add Qiskit sample and UT
    
    * record test
    
    * add bell state example
    
    * drop 3rd unused qubit
    
    * remove qiskit=>qir samples
    
    * add .bc file
    
    * update recording
    
    * update sample to use FullStateTarget
    
    Co-authored-by: Andres Paz <anpaz@microsoft.com>

---
+++ azure-quantum/tests/unit/test_simulator.py
@@ -0,0 +1,32 @@
+from json import JSONDecodeError
+import unittest
+import os
+import pytest
+from azure.quantum.target.microsoft.simulator.fullstate import FullStateTarget
+from azure.quantum.job.job import Job
+
+from common import QuantumTestBase, ZERO_UID
+
+
+class TestFullStateSimulator(QuantumTestBase):
+    mock_create_job_id_name = "create_job_id"
+    create_job_id = Job.create_job_id
+
+    def get_test_job_id(self):
+        return ZERO_UID if self.is_playback \
+               else Job.create_job_id()
+
+    def _test_qir_file(self):
+        return os.path.join(os.path.split(__file__)[0], "qir", "Sample.bc")
+
+    def test_job_submit_microsoft_full_state(self):
+        with unittest.mock.patch.object(
+            Job,
+            self.mock_create_job_id_name,
+            return_value=self.get_test_job_id(),
+        ):
+            workspace = self.create_workspace()
+            target: FullStateTarget = workspace.get_targets("microsoft.simulator.fullstate")
+            job = target.submit_qir_file(self._test_qir_file(), "QIR test", "Sample__HelloQ")
+            result: str = job.get_results()
+            assert result.startswith("Hello quantum world!")
