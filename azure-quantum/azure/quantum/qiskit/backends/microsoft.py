0becf64b23c00f4cfb6fcff4e1ec7407d2151b17
Mathias Soeken <mathias.soeken@gmail.com>
2022-09-19 14:37:41 -0700

    Update azure-quantum/azure/quantum/qiskit/backends/microsoft.py

--- azure-quantum/azure/quantum/qiskit/backends/microsoft.py
+++ azure-quantum/azure/quantum/qiskit/backends/microsoft.py
@@ -47,7 +47,7 @@ class MicrosoftResourcesEstimatorBackend(MicrosoftBackend):
 
     @classmethod
     def _default_options(cls):
-        return Options(entryPoint="main", arguments=[], targetCapibility="BasicExecution", errorBudget=1e-3, qubit={}, faultToleranceProtocol="surface_code")
+        return Options(entryPoint="main", arguments=[], targetCapability="BasicExecution", errorBudget=1e-3, qubit={}, faultToleranceProtocol="surface_code")
 
     def __init__(self, name: str, provider: "AzureQuantumProvider", **kwargs):
         """Constructor for class to interface with the resources estimator target"""
