510036e4fb022c9fd09cfe213016e0a2f22ea047
Mathias Soeken <mathias.soeken@outlook.com>
2022-10-20 17:54:55 +0200

    Summary view.

--- azure-quantum/azure/quantum/qiskit/results/resource_estimator.py
+++ azure-quantum/azure/quantum/qiskit/results/resource_estimator.py
@@ -14,6 +14,7 @@ class ResourceEstimatorResult(Result):
         **kwargs
     ) -> None:
         super().__init__(**kwargs)
+        self.summary = ResourceEstimatorResultSummary(**kwargs)
 
     def _repr_html_(self):
         return self._repr_html_success_() if self.success else self._repr_html_error_()
@@ -43,6 +44,93 @@ class ResourceEstimatorResult(Result):
                             <hr style="margin-top: 2px; margin-bottom: 0px; border-top: solid 1px black" />
                             {explanation}
                         </td>
+                    </tr>
+                """
+            html += "</table></details>"
+
+        html += f"<details> <summary style='display:list-item'><strong>Assumptions</strong></summary><ul>"
+        for assumption in self.data()['reportData']['assumptions']:
+            html += f"<li>{md.convert(assumption)}</li>"
+        html += "</ul></details>"
+
+        return html
+
+    def _repr_html_error_(self):
+        html = "<b>Job not successful</b>"
+        if self.error_data is not None:
+            html += f"""<br>Error code: {self.error_data['code']}
+                <br>Error message: {self.error_data['message']}"""
+        return html
+
+class ResourceEstimatorResultSummary(Result):
+    def __init__(
+        self,
+        **kwargs
+    ) -> None:
+        super().__init__(**kwargs)
+
+    def _repr_html_(self):
+        return self._repr_html_success_() if self.success else self._repr_html_error_()
+
+    def _repr_html_success_(self):
+        html = """
+            <style>
+                .aqre-tooltip {
+                    position: relative;
+                    border-bottom: 1px dotted black;
+                }
+
+                .aqre-tooltip .aqre-tooltiptext {
+                    font-weight: normal;
+                    visibility: hidden;
+                    width: 600px;
+                    background-color: #e0e0e0;
+                    color: black;
+                    text-align: center;
+                    border-radius: 6px;
+                    padding: 5px 5px;
+                    position: absolute;
+                    z-index: 1;
+                    top: 150%;
+                    left: 50%;
+                    margin-left: -200px;
+                    border: solid 1px black;
+                }
+
+                .aqre-tooltip .aqre-tooltiptext::after {
+                    content: "";
+                    position: absolute;
+                    bottom: 100%;
+                    left: 50%;
+                    margin-left: -5px;
+                    border-width: 5px;
+                    border-style: solid;
+                    border-color: transparent transparent black transparent;
+                }
+
+                .aqre-tooltip:hover .aqre-tooltiptext {
+                    visibility: visible;
+                }
+            </style>"""
+
+        md = markdown.Markdown(extensions=['mdx_math'])
+        for group in self.data()['reportData']['groups']:
+            html += f"""
+                <details {"open" if group['alwaysVisible'] else ""}>
+                    <summary style="display:list-item">
+                        <strong>{group['title']}</strong>
+                    </summary>
+                <table>"""
+            for entry in group['entries']:
+                val = self.data()
+                for key in entry['path'].split("/"):
+                    val = val[key]
+                explanation = md.convert(entry["explanation"])
+                html += f"""
+                    <tr class="aqre-tooltip">
+                        <td style="font-weight: bold"><span class="aqre-tooltiptext">{explanation}</span>{entry['label']}</td>
+                        <td>{val}</td>
+                        <td style="text-align: left">{entry["description"]}</td>
                     </tr>
                 """
             html += "</table></details>"
