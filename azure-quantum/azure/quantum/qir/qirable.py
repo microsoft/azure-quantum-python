cf7f65456a34269784507234eb5f551fe1f38aa3
XField <58103249+vxfield@users.noreply.github.com>
2023-01-30 17:35:39 -0800

    Add kwargs to the Qirable protocol

--- azure-quantum/azure/quantum/qir/qirable.py
+++ azure-quantum/azure/quantum/qir/qirable.py
@@ -3,12 +3,12 @@
 # Licensed under the MIT License.
 ##
 
-from typing import Optional, Dict, Protocol
+from typing import Optional, Dict, Protocol, Any
 from abc import abstractmethod
 
 __all__ = ["Qirable"]
 
 class Qirable(Protocol):
     @abstractmethod
-    def _repr_qir_(self, metadata: Optional[Dict[str, str]] = None) -> bytes:
+    def _repr_qir_(self, metadata: Optional[Dict[str, str]] = None, **kwargs: Any) -> bytes:
         raise NotImplementedError
