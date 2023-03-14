ec882111f49a94c056d253ceb341650a5753a026
Mathias Soeken <mathias.soeken@outlook.com>
2023-03-14 14:33:47 +0100

    Remove dedicated result class.

--- azure-quantum/azure/quantum/qiskit/results/resource_estimator.py
+++ azure-quantum/azure/quantum/qiskit/results/resource_estimator.py
@@ -9,29 +9,17 @@
 from azure.quantum.target.microsoft.result import MicrosoftEstimatorResult
 
 
-class ResourceEstimatorResult(MicrosoftEstimatorResult):
-    """
-    A customized result for a resource estimation job.
-    """
+def make_estimator_result(data):
+    if not data["success"]:
+        error_data = data["error_data"]
+        message = "Cannot retrieve results as job execution failed " \
+                  f"({error_data['code']}: {error_data['message']})"
 
-    def __init__(
-        self,
-        data
-    ) -> None:
-        super().__init__(data)
-
-    @classmethod
-    def from_dict(cls, data):
-        if not data["success"]:
-            error_data = data["error_data"]
-            message = "Cannot retrieve results as job execution failed " \
-                      f"({error_data['code']}: {error_data['message']})"
-
-            raise RuntimeError(message)
-
-        results = data["results"]
-        if len(results) == 1:
-            data = results[0]['data']
-            return ResourceEstimatorResult(data)
-        else:
-            raise ValueError("Expected Qiskit results for RE be of length 1")
+        raise RuntimeError(message)
+    
+    results = data["results"]
+    if len(results) == 1:
+        data = results[0]['data']
+        return MicrosoftEstimatorResult(data)
+    else:
+        raise ValueError("Expected Qiskit results for RE be of length 1")
