7ca76affea0ba646006fd66ffbfcf39090cf9dc4
XField <58103249+vxfield@users.noreply.github.com>
2023-03-22 08:45:37 -0700

    Fixes and more tests

--- azure-quantum/tests/unit/test_qsharp.py
+++ azure-quantum/tests/unit/test_qsharp.py
@@ -8,33 +8,31 @@ import warnings
 import pytest
 from azure.quantum.job import Job
 from common import QuantumTestBase, ZERO_UID
+from import_qsharp import skip_if_no_qsharp
+from test_job_payload_factory import JobPayloadFactory
 
 @pytest.mark.qsharp
 @pytest.mark.live_test
-@pytest.mark.skip(reason="Temporarily disabling these tests as they are failing the QDK build")
+@skip_if_no_qsharp
 class TestQSharpQIRJob(QuantumTestBase):
 
     @pytest.mark.rigetti
-    def test_qsharp_qir_rigetti(self):
-        self._run_job("rigetti.sim.qvm")
+    def test_qsharp_qir_inline_rigetti(self):
+        self._run_job("rigetti.sim.qvm", inline=True)
+
+    @pytest.mark.rigetti
+    def test_qsharp_qir_file_rigetti(self):
+        self._run_job("rigetti.sim.qvm", inline=False)
 
     @pytest.mark.quantinuum
-    def test_qsharp_qir_quantinuum(self):
-        self._run_job("quantinuum.sim.h1-1sc")
+    def test_qsharp_qir_inline_quantinuum(self):
+        self._run_job("quantinuum.sim.h1-1sc", inline=True)
 
-    @pytest.fixture(autouse=True, scope='class')
-    def _qsharp_callable(self):
-        import qsharp
-        TestQSharpQIRJob.qsharp_callable = qsharp.compile("""
-            open Microsoft.Quantum.Intrinsic;
-            operation GenerateRandomBit_Inline() : Result {
-                use q0 = Qubit();
-                H(q0);
-                return M(q0);
-            }
-        """)
+    @pytest.mark.quantinuum
+    def test_qsharp_qir_file_quantinuum(self):
+        self._run_job("quantinuum.sim.h1-1sc", inline=False)
 
-    def _run_job(self, target_name):
+    def _run_job(self, target_name, inline):
         with unittest.mock.patch.object(
             Job,
             "create_job_id",
@@ -42,7 +40,9 @@ class TestQSharpQIRJob(QuantumTestBase):
         ):
             workspace = self.create_workspace()
             target = workspace.get_targets(target_name)
-            job = target.submit(input_data=TestQSharpQIRJob.qsharp_callable)
+            input_data = (JobPayloadFactory.get_qsharp_inline_callable_bell_state()[0] if inline
+                          else JobPayloadFactory.get_qsharp_file_callable_bell_state()[0])
+            job = target.submit(input_data=input_data)
 
             self.pause_recording()
             try:
