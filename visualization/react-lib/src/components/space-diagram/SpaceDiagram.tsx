603db1c32c72e889d3235788473cb9bb58951c77
Katy McClintic <kamcclin@microsoft.com>
2023-04-26 22:39:39 -0700

    got hover working

--- visualization/react-lib/src/components/space-diagram/SpaceDiagram.tsx
+++ visualization/react-lib/src/components/space-diagram/SpaceDiagram.tsx
@@ -1,26 +1,31 @@
-
-import React from 'react';
-import DonutChart from '../d3-visualization-components/DonutChart';
-
+import React from "react";
+import DonutChart from "../d3-visualization-components/DonutChart";
+import "./SpaceDiagram.css";
 /*
 export type SpaceDiagramProps = {
   physicalQubitsAlgorithm: number;
   physicalQubitsTFactory: number;
 }*/
 
- //function SpaceDiagram({physicalQubitsAlgorithm, physicalQubitsTFactory}: SpaceDiagramProps): JSX.Element{
-  function SpaceDiagram() {
+//function SpaceDiagram({physicalQubitsAlgorithm, physicalQubitsTFactory}: SpaceDiagramProps): JSX.Element{
+function SpaceDiagram() {
   const data = [
-    {title: 'Physical algorithmic qubits', value: 100},
-    {title: 'Physical T-Factory qubits', value: 25}
+    { title: "Physical algorithmic qubits", value: 100 },
+    { title: "Physical T-Factory qubits", value: 25 },
   ];
 
   return (
     <div>
-      <DonutChart data={data}  width={500} height={500} innerRadius = {100} outerRadius = {250} color="green" />
+      <div className="space-diagram"> Space diagram </div>
+      <DonutChart
+        data={data}
+        width={1000}
+        height={1000}
+        innerRadius={150}
+        outerRadius={225}
+      />
     </div>
   );
-};
+}
 
 export default SpaceDiagram;
-
