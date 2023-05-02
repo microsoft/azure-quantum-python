01471b282041194af9cc0ca6e85946202c45de29
Frances Tibble <frtibble@microsoft.com>
2023-05-02 10:09:00 +0100

    Add deprecation notice to 1st party QIO targets

--- azure-quantum/azure/quantum/target/microsoft/qio/simulated_annealing.py
+++ azure-quantum/azure/quantum/target/microsoft/qio/simulated_annealing.py
@@ -2,6 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 ##
+import warnings
+warnings.warn("Please note that Microsoft QIO solvers will be deprecated and \
+no longer available in Azure Quantum after June 30th, 2023.")
+
 import logging
 from multiprocessing.sharedctypes import Value
 
