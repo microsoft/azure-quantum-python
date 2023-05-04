610f26469ec9ec1421c7bbcde2b36f50f433268a
XField <58103249+vxfield@users.noreply.github.com>
2023-05-03 22:09:54 -0700

    Install IQ# for live tests

--- azure-quantum/tests.live/Run.ps1
+++ azure-quantum/tests.live/Run.ps1
@@ -29,6 +29,21 @@ if ($True -eq $SkipInstall) {
 $EnvName = GetEnvName -PackageName $PackageName
 Use-CondaEnv $EnvName
 
+# Try installing IQ# dotnet tool, IQ# kernel and the qsharp Python package
+# Used for running tests between the Azure Quantum Python SDK and IQ# (Q#+QIR job submission)
+if ([string]::IsNullOrEmpty($PackageName) -or ($PackageName -eq "azure-quantum")) {
+    try {
+      $EnvExists = conda env list | Select-String -Pattern "azurequantum " | Measure-Object | Select-Object -Exp Count
+      if ($EnvExists) {
+        conda activate azurequantum
+        & (Join-Path $RootDir "build" "install-iqsharp.ps1");
+      }    
+    }
+    catch {
+      Write-Host "##[warning]Failed to install IQ#."
+    }
+}
+
 function PyTestMarkExpr() {
     param (
         [string[]] $AzureQuantumCapabilities
