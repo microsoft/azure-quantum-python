527b49b9917f263e09b458c1cff24ed48fdf0c25
Katy McClintic <8239561+katymccl@users.noreply.github.com>
2023-06-22 15:30:57 -0700

    updated build script

--- visualization/build/build.sh
+++ visualization/build/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Use flag buildJsLibOnly to only build js-lib
+
+if [ "$1" == "buildJsLibOnly" ]; then
+  cd ../js-lib
+  npm link react quantum-visualization
+  if [ $? -eq 0 ]; then
+    echo 'Successfully linked react and quantum-visualization'
+  fi
+  npm run build
+  if [ $? -eq 0 ]; then
+    echo 'Successfully built js-lib'
+ exit 0
+  fi
+ else 
 
 cd ../react-lib
 npm install
@@ -27,6 +41,5 @@ npm run build
 if [ $? -eq 0 ]; then
   echo 'Successfully built js-lib'
 fi
-
-
 echo 'Successfully built js-li and dependencies.' && exit 0
+fi
