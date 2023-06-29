a1092226525f2656a6ac822cdc223a8cd48afc1f
Frances Tibble <frtibble@microsoft.com>
2023-06-29 13:25:56 +0100

    remove import

--- azure-quantum/azure/quantum/target/microsoft/__init__.py
+++ azure-quantum/azure/quantum/target/microsoft/__init__.py
@@ -2,15 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 ##
-from .qio import (
-    ParallelTempering,
-    PopulationAnnealing,
-    QuantumMonteCarlo,
-    SimulatedAnnealing,
-    SubstochasticMonteCarlo,
-    Tabu,
-)
-
 __all__ = ["ErrorBudgetPartition", "MicrosoftEstimator",
            "MicrosoftEstimatorJob", "MicrosoftEstimatorResult",
            "MicrosoftEstimatorParams", "QECScheme", "QubitParams"]
