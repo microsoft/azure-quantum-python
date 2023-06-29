c992e47067a71f0e7edd91556d3968e6427c7722
Frances Tibble <frtibble@microsoft.com>
2023-06-29 14:18:37 +0100

    modify docs

--- azure-quantum/azure/quantum/job/base_job.py
+++ azure-quantum/azure/quantum/job/base_job.py
@@ -236,7 +236,7 @@ class BaseJob(WorkspaceItem):
         :type container_uri: str
         :param input_data: Input data in binary format
         :type input_data: bytes
-        :param content_type: Content type, e.g. "application/json" or "application/x-protobuf"
+        :param content_type: Content type, e.g. "application/json"
         :type content_type: Optional, ContentType
         :param blob_name: Blob name, defaults to "inputData"
         :type blob_name: str, optional
