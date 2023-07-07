9cd268e1d315c40ddad0dac781ff804175d8ad57
kamcclin <kamcclin@microsoft.com>
2023-07-07 15:03:33 -0700

    trying to exclude react-lib build from script and also use only one publish step

---
+++ visualization/build/build-jslib.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# assume working directory to be "visualization/build"
+
+cd ../react-lib
+npm link
+if [ $? -eq 0 ]; then
+  echo 'Successfully created link: react-lib'
+fi
+cd node_modules/react
+npm link
+if [ $? -eq 0 ]; then
+  echo 'Successfully created link: node_modules/react'
+fi
+cd ../../../js-lib
+npm link react quantum-visualization
+if [ $? -eq 0 ]; then
+  echo 'Successfully linked react and quantum-visualization to js-lib.'
+fi
+npm run build
+if [ $? -eq 0 ]; then
+  echo 'Successfully built js-lib'
+fi
+echo 'Successfully built js-lib and dependencies.' && exit 0
+fi
