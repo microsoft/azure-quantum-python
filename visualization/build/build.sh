1766af91a440d6ba9dc4851def3ec7149881d936
kamcclin <kamcclin@microsoft.com>
2023-07-07 14:35:49 -0700

    dont fail on babel errors

--- visualization/build/build.sh
+++ visualization/build/build.sh
@@ -25,21 +25,21 @@ if [ $? -eq 0 ]; then
 fi
 npm link
 if [ $? -eq 0 ]; then
-  echo 'Successfully linked react-lib'
+  echo 'Successfully created link" react-lib'
 fi
 cd node_modules/react
 npm link
 if [ $? -eq 0 ]; then
-  echo 'Successfully linked node_modules/react'
+  echo 'Successfully created link: node_modules/react'
 fi
 cd ../../../js-lib
 npm link react quantum-visualization
 if [ $? -eq 0 ]; then
-  echo 'Successfully linked react and quantum-visualization'
+  echo 'Successfully linked react and quantum-visualization to js-lib.'
 fi
 npm run build
 if [ $? -eq 0 ]; then
   echo 'Successfully built js-lib'
 fi
-echo 'Successfully built js-li and dependencies.' && exit 0
+echo 'Successfully built js-lib and dependencies.' && exit 0
 fi
