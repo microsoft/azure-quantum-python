e1da3ad1a6723f5d84b279aad1ec78edec4ad373
kamcclin <kamcclin@microsoft.com>
2023-07-07 14:53:27 -0700

    added babel support

--- visualization/js-lib/babel.config.js
+++ visualization/js-lib/babel.config.js
@@ -2,8 +2,10 @@ module.exports = function (api) {
     api.cache(true);
     
     const presets = [ "@babel/preset-env", "@babel/preset-react" ];
+    const generatorOpts ={"compact" : false};
 
     return {
       presets,
+      generatorOpts
     };
   }
