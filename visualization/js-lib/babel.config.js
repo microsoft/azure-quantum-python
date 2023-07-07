50e85294c166ce27719d36813fc75056d27aef66
kamcclin <kamcclin@microsoft.com>
2023-07-07 15:12:40 -0700

    changed compact to true

--- visualization/js-lib/babel.config.js
+++ visualization/js-lib/babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
     api.cache(true);
     
     const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-    const generatorOpts ={"compact" : false};
+    const generatorOpts ={"compact" : true};
 
     return {
       presets,
