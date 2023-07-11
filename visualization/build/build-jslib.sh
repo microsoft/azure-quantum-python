f586bf86eb34ba4febc83942c61b8e1effa3b139
kamcclin <kamcclin@microsoft.com>
2023-07-11 13:42:28 -0700

    added failure messages in build and cleaned up build

--- visualization/build/build-jslib.sh
+++ visualization/build/build-jslib.sh
@@ -1,34 +1,55 @@
-#!/bin/bash
+#!/bin/bash 
 # assume working directory to be "visualization/build"
 
 cd ../react-lib
 npm install
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
+  echo 'Failed to install: react-lib'
+  exit 1 
+else
   echo 'Successfully install: react-lib'
 fi
 
 npm run build
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
+  echo 'Failed to build: react-lib'
+  exit 1
+else 
   echo 'Successfully build: react-lib'
 fi
 
 npm link
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
+  echo 'Failed to create link: react-lib'
+  exit 1
+else
   echo 'Successfully created link: react-lib'
 fi
+
 cd node_modules/react
 npm link
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
+  echo 'Failed to create link: node_modules/react'
+  exit 1
+else
   echo 'Successfully created link: node_modules/react'
 fi
+
 cd ../../../js-lib
 npm link react quantum-visualization
-if [ $? -eq 0 ]; then
-  echo 'Successfully linked react and quantum-visualization to js-lib.'
+if [ $? -ne 0 ]; then
+  echo 'Failed to link react and quantum-visualization to js-lib.'
+  exit 1
+else
+  echo 'Successfully linked react and quantum-visualization to js-lib'
 fi
+
 npm run build
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
+  echo 'Failed to build js-lib'
+  exit 1
+else
   echo 'Successfully built js-lib'
 fi
+
 echo 'Successfully built js-lib and dependencies.' && exit 0
-fi
