8961fb66fe7bbefe349aacb5fdadc5d346787b8d
Xinyi Joffre <xiou@microsoft.com>
2023-11-01 10:30:44 -0700

    Add warning messages

---
+++ qdk/qdk/__init__.py
@@ -0,0 +1,2 @@
+import warnings
+warnings.warn("The `qdk.chemistry` package is no longer maintained. Please see the latest supported experiences for Azure Quantum here: https://learn.microsoft.com/azure/quantum/get-started-azure-quantum.")
