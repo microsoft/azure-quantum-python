3ae40d792868b7a62d7aa2a652123a0817c5d199
Yi Gu <guyi@microsoft.com>
2023-11-15 19:29:20 -0800

    Update test

--- azure-quantum/tests/unit/test_qiskit.py
+++ azure-quantum/tests/unit/test_qiskit.py
@@ -937,7 +937,7 @@ class TestQiskit(QuantumTestBase):
         config = backend.configuration()
         self.assertFalse(config.simulator)
         self.assertEqual(1, config.max_experiments)
-        self.assertEqual(80, config.num_qubits)
+        self.assertEqual(84, config.num_qubits)
         self.assertEqual("qir.v1", config.azure["content_type"])
         self.assertEqual("rigetti", config.azure["provider_id"])
         self.assertEqual("qir.v1", config.azure["input_data_format"])
