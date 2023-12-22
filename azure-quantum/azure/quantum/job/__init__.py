668d5acefd457d6ca3d0f7b2d821ea13bce0436f
Kirill Komissarov <kikomiss@microsoft.com>
2023-12-22 11:21:47 -0800

    chore: expose .job module

--- azure-quantum/azure/quantum/job/__init__.py
+++ azure-quantum/azure/quantum/job/__init__.py
@@ -8,3 +8,13 @@ from azure.quantum.job.workspace_item import WorkspaceItem
 from azure.quantum.job.workspace_item_factory import WorkspaceItemFactory
 from azure.quantum.job.session import Session, SessionHost
 from azure.quantum._client.models import JobDetails
+
+
+__all__=[
+    "Job", 
+    "WorkspaceItem", 
+    "WorkspaceItemFactory", 
+    "Session", 
+    "SessionHost", 
+    "JobDetails"
+    ]
