f493a127462e46bfab1c55a84a0e90a9f79d1ded
kikomiss <144282031+kikomiss@users.noreply.github.com>
2024-01-02 10:54:47 -0800

    feature: support for microsoft.dft target (#540)
    * feature: support for microsoft.dft target + allow to get failed job output

---
+++ azure-quantum/azure/quantum/target/microsoft/elements/dft/target.py
@@ -0,0 +1,54 @@
+from azure.quantum.job.base_job import ContentType
+from azure.quantum.job.job import Job
+from azure.quantum.target.target import Target
+from azure.quantum.workspace import Workspace
+from azure.quantum.target.params import InputParams
+from typing import Any, Dict, Type, Union
+from .job import MicrosoftElementsDftJob
+
+
+class MicrosoftElementsDft(Target):
+    """
+    Microsoft Elements Dft target from the microsoft-elements provider.
+    """
+
+    target_names = [
+        "microsoft.dft"
+    ]
+
+
+    def __init__(
+        self,
+        workspace: "Workspace",
+        name: str = "microsoft.dft",
+        **kwargs
+    ):
+        # There is only a single target name for this target
+        assert name == self.target_names[0]
+
+        # make sure to not pass argument twice
+        kwargs.pop("provider_id", None)
+
+        super().__init__(
+            workspace=workspace,
+            name=name,
+            input_data_format="microsoft.xyz.v1",
+            output_data_format="microsoft.dft-results.v1",
+            provider_id="microsoft-elements",
+            content_type=ContentType.text_plain,
+            **kwargs
+        )
+
+
+    def submit(self,
+               input_data: Any,
+               name: str = "azure-quantum-dft-job",
+               input_params: Union[Dict[str, Any], InputParams, None] = None,
+               **kwargs) -> MicrosoftElementsDftJob:
+        
+        return super().submit(input_data, name, input_params, **kwargs)
+
+
+    @classmethod
+    def _get_job_class(cls) -> Type[Job]:
+        return MicrosoftElementsDftJob
