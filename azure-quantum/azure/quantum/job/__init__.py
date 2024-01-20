fdcdeb55923dd6f60590208063cd9c669901915a
Kirill Komissarov <kikomiss@microsoft.com>
2024-01-19 18:52:37 -0800

    fix: include Session and SessionHost

--- azure-quantum/azure/quantum/job/__init__.py
+++ azure-quantum/azure/quantum/job/__init__.py
@@ -12,11 +12,17 @@ from azure.quantum.job.job import Job
 from azure.quantum.job.job_failed_with_results_error import JobFailedWithResultsError
 from azure.quantum.job.workspace_item import WorkspaceItem
 from azure.quantum.job.workspace_item_factory import WorkspaceItemFactory
+from azure.quantum.job.session import Session, SessionHost, SessionDetails, SessionStatus, SessionJobFailurePolicy
 
 __all__ = [
     "Job",
     "JobDetails",
     "BaseJob",
     "FilteredJob",
-    "WorkspaceItem"
+    "WorkspaceItem",
+    "Session",
+    "SessionHost",
+    "SessionDetails",
+    "SessionStatus",
+    "SessionJobFailurePolicy"
     ]
