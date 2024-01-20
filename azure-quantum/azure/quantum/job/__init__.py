584e26cc246c7c446726a25cde634a540052e3c5
Kirill Komissarov <kikomiss@microsoft.com>
2024-01-19 19:05:28 -0800

    fix

--- azure-quantum/azure/quantum/job/__init__.py
+++ azure-quantum/azure/quantum/job/__init__.py
@@ -6,23 +6,18 @@
 """Defines Azure Quantum job model"""
 
 from azure.quantum._client.models import JobDetails
-from azure.quantum.job.base_job import BaseJob
-from azure.quantum.job.filtered_job import FilteredJob
-from azure.quantum.job.job import Job
-from azure.quantum.job.job_failed_with_results_error import JobFailedWithResultsError
-from azure.quantum.job.workspace_item import WorkspaceItem
-from azure.quantum.job.workspace_item_factory import WorkspaceItemFactory
-from azure.quantum.job.session import Session, SessionHost, SessionDetails, SessionStatus, SessionJobFailurePolicy
+from .base_job import BaseJob
+from .filtered_job import FilteredJob
+from .job import Job
+from .job_failed_with_results_error import JobFailedWithResultsError
+from .workspace_item import WorkspaceItem
+from .workspace_item_factory import WorkspaceItemFactory
+from .session import Session, SessionHost, SessionDetails, SessionStatus, SessionJobFailurePolicy
 
 __all__ = [
     "Job",
     "JobDetails",
     "BaseJob",
     "FilteredJob",
-    "WorkspaceItem",
-    "Session",
-    "SessionHost",
-    "SessionDetails",
-    "SessionStatus",
-    "SessionJobFailurePolicy"
+    "WorkspaceItem"
     ]
