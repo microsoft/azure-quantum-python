63f8b0269b83b5efe266e0087ab1176bc5d2d685
Xinyi Joffre <xiou@microsoft.com>
2024-02-05 14:50:58 -0800

    Deprecate Toshiba solvers (#561)
    * Deprecate Toshiba solvers
    ---------
    
    Co-authored-by: kikomiss <144282031+kikomiss@users.noreply.github.com>
    Co-authored-by: Kirill Komissarov <kikomiss@microsoft.com>

--- azure-quantum/azure/quantum/target/__init__.py
+++ azure-quantum/azure/quantum/target/__init__.py
@@ -6,10 +6,6 @@
 """Defines set of targets for interacting with Azure Quantum"""
 
 from .target import Target
-from .solvers import Solver
-from .toshiba import (
-    SimulatedBifurcationMachine
-)
 from .ionq import IonQ
 from .quantinuum import Quantinuum
 from .rigetti import Rigetti
@@ -22,7 +18,6 @@ DEFAULT_TARGETS = {
     "ionq": IonQ,
     "quantinuum": Quantinuum,
     "rigetti": Rigetti,
-    "toshiba": Solver,
     "pasqal": Pasqal
 }
 
