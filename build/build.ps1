bd4fc51af332079c4a5447dfd59896a858a452fb
Xinyi Joffre <xiou@microsoft.com>
2024-02-07 18:10:07 -0800

    Remove old CI build and replace with simpler CI (#570)

--- build/build.ps1
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-<#
-    .SYNOPSIS
-        Build: No build step required
-#>
-
-Write-Host "##[info]No build step required."
