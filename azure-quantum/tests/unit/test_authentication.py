59211a6ed3af62e3b0259cdc409955823ae81f7a
Kirill Komissarov <kikomiss@microsoft.com>
2024-05-18 20:28:14 -0700

    chore: fix unit-tests

--- azure-quantum/tests/unit/test_authentication.py
+++ azure-quantum/tests/unit/test_authentication.py
@@ -121,28 +121,28 @@ class TestWorkspace(QuantumTestBase):
 
     @pytest.mark.live_test
     def test_workspace_auth_token_credential(self):
-        with patch.dict(os.environ):
-            self.clear_env_vars(os.environ)
-            connection_params = self.connection_params
-            credential = DefaultAzureCredential()
-            token = credential.get_token(ConnectionConstants.DATA_PLANE_CREDENTIAL_SCOPE)
-            content = {
-                "access_token": token.token,
-                "expires_on": token.expires_on * 1000
-            }
-            tmpdir = self.create_temp_dir()
-            file = Path(tmpdir) / "token.json"
-            try:
-                file.write_text(json.dumps(content))
-                with patch.dict(os.environ,
-                                {EnvironmentVariables.QUANTUM_TOKEN_FILE: str(file.resolve())},
-                                clear=True):
-                    credential = _TokenFileCredential()
-                    workspace = self.create_workspace(credential=credential)
-                    targets = workspace.get_targets()
-                    self.assertGreater(len(targets), 1)
-            finally:
-                os.remove(file)
+        connection_params = self.connection_params
+        credential = _DefaultAzureCredential(            
+            subscription_id=connection_params.subscription_id,
+            arm_endpoint=connection_params.arm_endpoint)
+        token = credential.get_token(ConnectionConstants.DATA_PLANE_CREDENTIAL_SCOPE)
+        content = {
+            "access_token": token.token,
+            "expires_on": token.expires_on * 1000
+        }
+        tmpdir = self.create_temp_dir()
+        file = Path(tmpdir) / "token.json"
+        try:
+            file.write_text(json.dumps(content))
+            with patch.dict(os.environ,
+                            {EnvironmentVariables.QUANTUM_TOKEN_FILE: str(file.resolve())},
+                            clear=True):
+                credential = _TokenFileCredential()
+                workspace = self.create_workspace(credential=credential)
+                targets = workspace.get_targets()
+                self.assertGreater(len(targets), 1)
+        finally:
+            os.remove(file)
 
     @pytest.mark.live_test
     @pytest.mark.skip(reason="Only to be used in manual testing when secret is provided")
@@ -160,19 +160,13 @@ class TestWorkspace(QuantumTestBase):
 
     @pytest.mark.live_test
     def test_workspace_auth_default_credential(self):
-        with patch.dict(os.environ):
-            self.clear_env_vars(os.environ)
-            connection_params = self.connection_params
-            os.environ[EnvironmentVariables.AZURE_CLIENT_ID] = \
-                connection_params.client_id
-            os.environ[EnvironmentVariables.AZURE_TENANT_ID] = \
-                connection_params.tenant_id
-            credential = _DefaultAzureCredential(
-                subscription_id=connection_params.subscription_id,
-                arm_endpoint=connection_params.arm_endpoint)
-            workspace = self.create_workspace(credential=credential)
-            targets = workspace.get_targets()
-            self.assertGreater(len(targets), 1)
+        connection_params = self.connection_params
+        credential = _DefaultAzureCredential(
+            subscription_id=connection_params.subscription_id,
+            arm_endpoint=connection_params.arm_endpoint)
+        workspace = self.create_workspace(credential=credential)
+        targets = workspace.get_targets()
+        self.assertGreater(len(targets), 1)
 
     @pytest.mark.skip(reason="Only to be used in manual testing")
     @pytest.mark.live_test
