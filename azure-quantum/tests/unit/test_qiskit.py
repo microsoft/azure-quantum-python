db8a1842fdf9e8f44017ec36277fa69cf28a82b0
Kirill Komissarov <kikomiss@microsoft.com>
2024-09-30 15:05:17 -0700

    fix: add test_qiskit_endiannes_submit_to_quantinuum recording

--- azure-quantum/tests/unit/test_qiskit.py
+++ azure-quantum/tests/unit/test_qiskit.py
@@ -1836,14 +1836,14 @@ class TestQiskit(QuantumTestBase):
         self.assertEqual("qir.v1", config.azure["input_data_format"])
         self.assertEqual("microsoft.quantum-results.v2", backend._get_output_data_format())
 
-    @pytest.mark.qci
+    @pytest.mark.quantinuum
     @pytest.mark.live_test
-    def test_qiskit_endianness_submit_to_qci(
+    def test_qiskit_endianness_submit_to_quantinuum(
         self, expectation="000 000 001"
     ):
         workspace = self.create_workspace()
         provider = AzureQuantumProvider(workspace=workspace)
-        backend = provider.get_backend("qci.simulator")
+        backend = provider.get_backend("quantinuum.sim.h1-1e")
         shots = 100
 
         qr = QuantumRegister(3)
