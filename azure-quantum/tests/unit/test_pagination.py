8903565e9717011b91347738c7a6954d2e55ec68
Yi Gu <guyi@microsoft.com>
2025-02-06 17:37:47 -0800

    update tests

--- azure-quantum/tests/unit/test_pagination.py
+++ azure-quantum/tests/unit/test_pagination.py
@@ -36,7 +36,7 @@ class TestWorkspacePagination(QuantumTestBase):
                                           created_after=datetime(2024, 10, 1),
                                           created_before=datetime(2024, 11, 1))
         # pylint: enable=protected-access
-        expected = "startswith%28Name%2C%20%27name%27%29%20and%20%28%24ItemType%20eq%20%27Session%27%20or%20ItemType%20eq%20%27Job%27%29%20and%20%28JobType%20eq%20%27Regular%27%20or%20JobType%20eq%20%27Chemistry%27%29%20and%20%28ProviderId%20eq%20%27ionq%27%20or%20ProviderId%20eq%20%27quantinuum%27%29%20and%20%28Target%20eq%20%27ionq.sim%27%20or%20Target%20eq%20%27quantinuum%2Csim%27%29%20and%20%28State%20eq%20%27Completed%27%20or%20State%20eq%20%27Failed%27%29%20and%20CreationTime%20ge%202024-10-01%20and%20CreationTime%20le%202024-11-01"
+        expected = "startswith(Name, 'name') and (ItemType eq 'Session' or ItemType eq 'Job') and (JobType eq 'Regular' or JobType eq 'Chemistry') and (ProviderId eq 'ionq' or ProviderId eq 'quantinuum') and (Target eq 'ionq.sim' or Target eq 'quantinuum,sim') and (State eq 'Completed' or State eq 'Failed') and CreationTime ge 2024-10-01T00:00:00 and CreationTime le 2024-11-01T00:00:00"
         self.assertEqual(filter_string, expected)
 
     def test_orderby_valid(self):
@@ -53,14 +53,14 @@ class TestWorkspacePagination(QuantumTestBase):
             # pylint: disable=protected-access
             orderby = ws._create_orderby(var_name, True)
             # pylint: enable=protected-access
-            expected = var_name + "%20"+"asc"
+            expected = var_name + " "+"asc"
             self.assertEqual(orderby, expected)
 
         for var_name in var_names:
             # pylint: disable=protected-access
             orderby = ws._create_orderby(var_name, False)
             # pylint: enable=protected-access
-            expected = var_name + "%20"+"desc"
+            expected = var_name + " "+"desc"
             self.assertEqual(orderby, expected)
 
     def test_orderby_invalid(self):
