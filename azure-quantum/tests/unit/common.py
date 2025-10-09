e24b9ea01c20eb599f56a1fe61ca51f7773417bc
Kirill Komissarov <kikomiss@microsoft.com>
2025-10-09 16:50:26 -0700

    fix: latest test recordings + use EnvironmentCredential for tests

--- azure-quantum/tests/unit/common.py
+++ azure-quantum/tests/unit/common.py
@@ -26,7 +26,7 @@ from azure.quantum._constants import (
 )
 from azure.quantum import Job
 from azure.quantum.target import Target
-from azure.identity import ClientSecretCredential, AzureCliCredential
+from azure.identity import ClientSecretCredential, EnvironmentCredential
 
 
 ZERO_UID = "00000000-0000-0000-0000-000000000000"
@@ -304,7 +304,7 @@ class QuantumTestBase(ReplayableTest):
                 client_secret=PLACEHOLDER)
 
         workspace = Workspace(
-            credential=credential if credential else AzureCliCredential(),
+            credential=credential if credential else EnvironmentCredential(),
             subscription_id=connection_params.subscription_id,
             resource_group=connection_params.resource_group,
             name=connection_params.workspace_name,
